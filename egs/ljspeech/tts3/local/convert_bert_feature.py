e54ccf587681c7af66bdcf7816dc74a99ad9fa15
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-02-01 15:22:59 +0900

    updated bert extraction scripts

--- egs/ljspeech/tts3/local/convert_bert_feature.py
+++ egs/ljspeech/tts3/local/convert_bert_feature.py
@@ -4,12 +4,13 @@
 import argparse
 import json
 import logging
-import multiprocessing as mp
 import os
+
+from distutils.util import strtobool
 
 import numpy as np
 
-import kaldi_io_py
+from espnet.utils.cli_utils import FileWriterWrapper
 
 
 def load_json(json_name):
@@ -52,35 +53,27 @@ def load_json(json_name):
     return jss
 
 
-def _write_json_as_arkscp(uids, jss, outid):
-    # write to ark and scp file (see https://github.com/vesis84/kaldi-io-for-python)
-    arkscp = 'ark:| copy-vector --print-args=false ark:- ark,scp:%s.ark,%s.scp' % (outid, outid)
-
-    # convert to ark, scp, and num_frame files
-    with kaldi_io_py.open_or_fd(arkscp, 'wb') as f:
-        for idx, (uid, js) in enumerate(zip(uids, jss), 1):
-            feats = np.array(js["features"][0]["layers"][0]["values"]).reshape(-1).astype(np.float32)
-            kaldi_io_py.write_vec_flt(f, feats, uid)
-            logging.info("(%d/%d) %s" % (idx, len(uids), uid))
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--nj", type=int, default=32, help="number of jobs")
+    parser.add_argument('--compress', type=strtobool, default=False,
+                        help='Save in compressed format')
+    parser.add_argument('--compression-method', type=int, default=2,
+                        help='Specify the method(if mat) or gzip-level(if hdf5)')
+    parser.add_argument('--write-num-frames', type=str,
+                        help='Specify wspecifer for utt2num_frames')
+    parser.add_argument('--filetype', type=str, default='mat',
+                        choices=['mat', 'hdf5'],
+                        help='Specify the file format for output. '
+                             '"mat" is the matrix format in kaldi')
     parser.add_argument("text", type=str, help="kaldi format text")
     parser.add_argument("json", type=str, help="json extracted by bert")
-    parser.add_argument("out", type=str, help="output file id")
+    parser.add_argument('wspecifier', type=str, help='Write specifier')
     args = parser.parse_args()
 
     # logging info
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s")
-
-    # chech direcitory
-    outdir = os.path.dirname(args.out)
-    if len(outdir) != 0 and not os.path.exists(outdir):
-        os.makedirs(outdir)
 
     # load text and json
     with open(args.text, "r") as f:
@@ -96,23 +89,19 @@ def main():
 
     assert len(uids) == len(jss)
 
-    # split uids and json
-    uids_list = np.array_split(uids, args.nj)
-    uids_list = [uids_.tolist() for uids_ in uids_list]
-    jss_list = np.array_split(jss, args.nj)
-    jss_list = [jss_.tolist() for jss_ in jss_list]
+    with FileWriterWrapper(args.wspecifier,
+                           filetype=args.filetype,
+                           write_num_frames=args.write_num_frames,
+                           compress=args.compress,
+                           compression_method=args.compression_method
+                           ) as writer:
+        for idx, (uid, js) in enumerate(zip(uids, jss), 1):
+            n_tokens = len(js["features"])
+            bert_feats = [np.array(js["features"][idx]["layers"][0]["values"]) for idx in range(1, n_tokens)]
+            bert_feats = np.stack(bert_feats).astype(np.float32)
+            writer[uid] = bert_feats
+            logging.info("(%d/%d) %s" % (idx, len(uids), uid))
 
-    # convert with multi-processing
-    processes = []
-    for idx, (uids_, jss_) in enumerate(zip(uids_list, jss_list), 1):
-        outid = args.out + ".%d" % (idx)
-        p = mp.Process(target=_write_json_as_arkscp, args=(uids_, jss_, outid))
-        p.start()
-        processes.append(p)
-
-    # wait for all process
-    for p in processes:
-        p.join()
 
 
 if __name__ == '__main__':
--- egs/ljspeech/tts3/local/extract_bert_feature.sh
+++ egs/ljspeech/tts3/local/extract_bert_feature.sh
@@ -1,34 +1,84 @@
 #!/bin/bash -e
 
-nj=32
-decode_sets="train_no_dev train_dev eval"
-bert_base_dir=exp/bert_uncased_L-24_H-1024_A-16
-script=/work4/t_hayashi/work/bert/extract_features.py
+nj=4
+cmd=run.pl
+write_utt2num_frames=true
+compress=true
+filetype=mat # mat or hdf5
 
-if [ ! -e ${bert_base_dir} ];then
-    wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip
-    unzip uncased_L-24_H-1024_A-16.zip
-    mv uncased_L-24_H-1024_A-16 ${bert_base_dir}
+. utils/parse_options.sh || exit 1
+
+set -euo pipefail
+
+if [ ! $# -eq 2 ]; then
+   echo "Usage: $0 [options] <data-dir> <bert-dir> ";
+   echo "e.g.: $0 data/train exp/bert"
+   echo "Options: "
+   echo "  --nj <nj>                                        # number of parallel jobs"
+   echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
+   exit 1;
 fi
 
-for decode_set in ${decode_sets};do
-    outdir=${bert_base_dir}/${decode_set}
-    [ ! -e ${outdir} ] && mkdir -p ${outdir}
-    cat data/${decode_set}/text | awk '{print $2}' > ${outdir}/input.txt
-    python ${script} \
-        --input_file=${outdir}/input.txt \
-        --output_file=${outdir}/output.json \
-        --vocab_file=${bert_base_dir}/vocab.txt \
-        --bert_config_file=${bert_base_dir}/bert_config.json \
-        --init_checkpoint=${bert_base_dir}/bert_model.ckpt \
+data=$1
+bertdir=$2
+
+# check bert installation
+script=../../../tools/bert/extract_features.py
+if [ ! -e ${script} ]; then
+    echo "It seems that bert is not installed."
+    echo "Please try following commands:"
+    echo "cd ../../../tools; make bert.done"
+    exit 1
+fi
+
+if [ ! -e ${bertdir} ];then
+    wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip
+    unzip uncased_L-24_H-1024_A-16.zip
+    mv uncased_L-24_H-1024_A-16 ${bertdir}
+fi
+
+name=$(basename ${data})
+outdir=${bertdir}/${name}
+[ ! -e ${outdir} ] && mkdir -p ${outdir}
+
+# split text into the nj subsets
+split_texts=""
+for n in $(seq ${nj}); do
+    split_texts="${split_texts} ${outdir}/text.${n}"
+done
+utils/split_scp.pl ${data}/text ${split_texts} || exit 1;
+for n in $(seq ${nj}); do
+    cat ${outdir}/text.${n} | cut -d " " -f 2- > ${outdir}/input.${n}.txt
+done
+
+# extract bert feature as json
+${cmd} --gpu 1 JOB=1:${nj} ${outdir}/log/extract_bert_${name}.JOB.log \
+     python ${script} \
+        --input_file=${outdir}/input.JOB.txt \
+        --output_file=${outdir}/output.JOB.json \
+        --vocab_file=${bertdir}/vocab.txt \
+        --bert_config_file=${bertdir}/bert_config.json \
+        --init_checkpoint=${bertdir}/bert_model.ckpt \
         --layers=-1 \
         --max_seq_length=128 \
         --batch_size=8
 
-    python local/convert_bert_feature.py --nj ${nj} \
-        data/${decode_set}/text ${outdir}/output.json ${outdir}/bert_vector
+# convert json to ark and scp
+if ${write_utt2num_frames}; then
+  write_num_frames_opt="--write-num-frames=ark,t:${outdir}/utt2num_frames.JOB"
+else
+  write_num_frames_opt=
+fi
 
-    for n in $(seq $nj); do
-        cat ${outdir}/bert_vector.$n.scp || exit 1;
-    done > ${outdir}/bert_vector.scp || exit 1
-done
+${cmd} --gpu 0 JOB=1:${nj} ${outdir}/log/convert_from_json_${name}.JOB.log \
+    python local/convert_bert_feature.py \
+        ${write_num_frames_opt} \
+        --compress=${compress} \
+        --filetype ${filetype} \
+        ${outdir}/text.JOB \
+        ${outdir}/output.JOB.json \
+        ark,scp:${outdir}/bert_feats.JOB.ark,${outdir}/bert_feats.JOB.scp
+
+for n in $(seq $nj); do
+    cat ${outdir}/bert_feats.${n}.scp || exit 1;
+done > ${outdir}/bert_feats.scp || exit 1
