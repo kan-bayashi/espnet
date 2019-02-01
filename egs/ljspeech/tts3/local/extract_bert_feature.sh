848ff33e3ad3fbadbe3d571d4b1ccb783c48b771
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-02-01 15:55:21 +0900

    added rm

--- egs/ljspeech/tts3/local/extract_bert_feature.sh
+++ egs/ljspeech/tts3/local/extract_bert_feature.sh
@@ -35,6 +35,7 @@ if [ ! -e ${bertdir} ];then
     wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip
     unzip uncased_L-24_H-1024_A-16.zip
     mv uncased_L-24_H-1024_A-16 ${bertdir}
+    rm uncased_L-24_H-1024_A-16.zip
 fi
 
 name=$(basename ${data})
