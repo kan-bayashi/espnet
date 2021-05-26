fdcbbfb8e5bc389aa9b515503c721473291e0908
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-05-26 23:36:20 +0900

    support threshold option

--- egs/clotho/asr1/local/back_translate_data_dir.sh
+++ egs/clotho/asr1/local/back_translate_data_dir.sh
@@ -6,6 +6,7 @@
 
 tgt_lang=en
 inter_lang=es
+edit_distance_threshold=-1
 
 # shellcheck disable=SC1091
 . utils/parse_options.sh || exit 1
@@ -16,7 +17,8 @@ if [ $# -ne 1 ]; then
   echo "Usage: $0 [options] <data_dir>"
   echo "Options:"
   echo "  --tgt_lang: Target language (default=${tgt_lang})"
-  echo "  --inter_lang: Intermediate languagte (default=${inter_lang})"
+  echo "  --inter_lang: Intermediate language (default=${inter_lang})"
+  echo "  --edit_distance_threshold: Threshold of edit distance (default=${edit_distance_threshold})"
   exit 1
 fi
 
@@ -26,6 +28,7 @@ utils/copy_data_dir.sh "${src_data_dir}" "${src_data_dir}_${inter_lang}2${tgt_la
 local/back_translate_text.py \
     --tgt_lang "${tgt_lang}" \
     --inter_lang "${inter_lang}" \
+    --edit_distance_threshold "${edit_distance_threshold}" \
     "${src_data_dir}/text" \
     "${src_data_dir}_${inter_lang}2${tgt_lang}/text"
 
